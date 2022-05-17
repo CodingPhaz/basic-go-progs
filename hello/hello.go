ee03e22e8e881d378539377276f0334c6d82dcaf
Samuel Bouye <bouyesamuel@gmail.com>
2022-05-17 16:56:15 +0000

    take user input for get name

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,12 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("vini")
+	var name string
+
+	fmt.Printf("Give your name: ")
+	fmt.Scan(&name)
+
+	message, err := greetings.Hello(name)
 	if err != nil {
 		log.Fatal(err)
 	}
